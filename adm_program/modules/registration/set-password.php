23b80b4075560366bf0b0caf4a70edbb6a24299a
Akshay Jumbade <akshay.spryox@gmail.com>
2018-07-31 13:25:13 +0530

    Working password strength indicator

--- adm_program/modules/registration/set-password.php
+++ adm_program/modules/registration/set-password.php
@@ -32,18 +32,17 @@ $page->addHtml('<script src="' . ADMIDIO_URL . '/adm_program/modules/registratio
 $page->addHtml('<script src="' . ADMIDIO_URL . '/adm_program/libs/zxcvbn/dist/zxcvbn.js"></script>');
 $passwordStrenthScript = <<<HTML
 <script>
-$("#admidio-password-strength-minimum").css("margin-left", "calc(" + $("#admidio-password-strength").css("width") + " / 4 * 1)");
-
-$("#new_password").keyup(function(e) {
-    var result = zxcvbn(e.target.value, []);
-    var cssClasses = ["progress-bar-danger", "progress-bar-danger", "progress-bar-warning", "progress-bar-info", "progress-bar-success"];
-
-    var progressBar = $("#admidio-password-strength .progress-bar");
-    progressBar.attr("aria-valuenow", result.score * 25);
-    progressBar.css("width", result.score * 25 + "%");
-    progressBar.removeClass(cssClasses.join(" "));
-    progressBar.addClass(cssClasses[result.score]);
-});
+$(document).ready(function(){
+    $("#admidio-password-strength-minimum").css("margin-left", "calc(" + $("#admidio-password-strength").css("width") + " / 4 * 1)");
+    $("#new_password").keyup(function(e) {
+        var result = zxcvbn(e.target.value, []);
+        var cssClasses = ["progress-bar-danger", "progress-bar-danger", "progress-bar-warning", "progress-bar-info", "progress-bar-success"];
+        progressBar.attr("aria-valuenow", result.score * 25);
+        progressBar.css("width", result.score * 25 + "%");
+        progressBar.removeClass(cssClasses.join(" "));
+        progressBar.addClass(cssClasses[result.score]);
+    });
+})
 </script>
 HTML;
 
